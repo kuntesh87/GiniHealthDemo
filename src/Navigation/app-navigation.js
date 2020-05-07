12bf96df90eac1a7cad4d8d3c15d3c92fd37645a
Kuntesh Patel <khpatel@gmail.com>
2020-05-07 14:10:46 -0300

    fixed eslint

--- src/Navigation/app-navigation.js
+++ src/Navigation/app-navigation.js
@@ -1,23 +1,8 @@
 import React from 'react';
-import Header from '../Components/header';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import Home from '../Screen/Home';
 // Declare Default Header Component
-const navigationOptions = ({navigation}) => ({
-  header: (
-    <Header
-      navigationProps={navigation}
-      hideRightIcon={navigation.getParam('hideRightIcon')}
-      hideLeftIcon={navigation.getParam('hideLeftIcon')}
-      title={navigation.getParam('headerTitle')}
-      titleElement={navigation.getParam('titleElement')}
-      hasSubheader={navigation.getParam('subheader')}
-      isBack={navigation.getParam('isBack')}
-      previousScreen={navigation.getParam('previousScreen')}
-    />
-  ),
-});
 
 const Stack = createStackNavigator();
 
@@ -25,7 +10,7 @@ const App = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={Home}  />
+        <Stack.Screen name="Home" component={Home} />
       </Stack.Navigator>
     </NavigationContainer>
   );
