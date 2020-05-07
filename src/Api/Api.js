113385ae3c2445c67945a4a3db4dd1c91d1d062a
Kuntesh Patel <khpatel@gmail.com>
2020-05-07 14:03:37 -0300

    search food

--- src/Api/Api.js
+++ src/Api/Api.js
@@ -6,9 +6,8 @@ export const getAnalysisForFood = async () => {
   try {
     const url = api + '/analyseNutritionixTest';
     const response = await axios.get(url, {
-      params: {
-        data: '1 cup mashed potatoes',
-      },
+      data: 'for breakfast i ate 2 eggs, bacon, and french toast',
+      timezone: 'US/Eastern',
       headers: {Authorization: 'Basic 8c74efc6bd0bb10200d38237e1905d6b'},
     });
     console.log('response', response);
@@ -19,9 +18,47 @@ export const getAnalysisForFood = async () => {
 
 getAnalysisForFood().then().catch();
 
+// get current log
 export const getLogTest = async () => {
   try {
     const url = api + '/getLogTest';
+    const response = await axios.get(url, {
+      headers: {Authorization: 'Basic 8c74efc6bd0bb10200d38237e1905d6b'},
+    });
+    console.log('response', response);
+  } catch (err) {
+    console.log('error', err);
+  }
+};
+
+// add food test
+export const logFoodTest = async () => {
+  try {
+    const url = api + '/logFoodTest';
+    const response = await axios.get(url, {
+      headers: {Authorization: 'Basic 8c74efc6bd0bb10200d38237e1905d6b'},
+    });
+    console.log('response', response);
+  } catch (err) {
+    console.log('error', err);
+  }
+};
+
+export const deleteLogTest = async () => {
+  try {
+    const url = api + '/deleteLogTest';
+    const response = await axios.get(url, {
+      headers: {Authorization: 'Basic 8c74efc6bd0bb10200d38237e1905d6b'},
+    });
+    console.log('response', response);
+  } catch (err) {
+    console.log('error', err);
+  }
+};
+
+export const updateLogTest = async () => {
+  try {
+    const url = api + '/updateLogTest';
     const response = await axios.get(url, {
       headers: {Authorization: 'Basic 8c74efc6bd0bb10200d38237e1905d6b'},
     });
